1e995a87942791adfe895e0d53e86ca30bb2478b
R. Modi <rajatmodi62@gmail.com>
2020-08-28 11:32:11 +0530

    fix the loading and testing of the combined/conditioned code

--- run.py
+++ run.py
@@ -1,17 +1,30 @@
 
 import os 
 
-# os.system("python train_single_dataset.py --training_type cifar --n_epochs 350")
-# os.system("python train_single_dataset.py --training_type fashion_mnist --n_epochs 350")
-# os.system("python train_combined.py --training_type combined --n_epochs 700")
 
-
+######################## SINGLE TRAINING ###########################################################################################
 #######cifar 
-# os.system("python train_single_dataset.py --lr 0.1")
-# os.system("python train_single_dataset.py --checkpoint_path experiments/cifar/checkpoint.pth --lr 0.01")
-# os.system("python train_single_dataset.py --checkpoint_path experiments/cifar/checkpoint.pth --lr 0.001")
+#os.system("python train_single_dataset.py --lr 0.1")
+#os.system("python train_single_dataset.py --checkpoint_path experiments/cifar/checkpoint.pth --lr 0.01")
+#os.system("python train_single_dataset.py --checkpoint_path experiments/cifar/checkpoint.pth --lr 0.001")
 
 ###fashion-mnist 
-os.system("python train_single_dataset.py --training_type fashion_mnist --lr 0.1")
-os.system("python train_single_dataset.py --training_type fashion_mnist --checkpoint_path experiments/fashion_mnist/checkpoint.pth --lr 0.01")
-os.system("python train_single_dataset.py --training_type fashion_mnist --checkpoint_path experiments/fashion_mnist/checkpoint.pth --lr 0.001")
+# os.system("python train_single_dataset.py --training_type fashion_mnist --lr 0.1")
+# os.system("python train_single_dataset.py --training_type fashion_mnist --checkpoint_path experiments/fashion_mnist/checkpoint.pth --lr 0.01")
+# os.system("python train_single_dataset.py --training_type fashion_mnist --checkpoint_path experiments/fashion_mnist/checkpoint.pth --lr 0.001")
+
+
+
+######################## COMBINED TRAINING ###########################################################################################
+##combined training 
+
+os.system("python train_combined.py  --lr 0.1")
+os.system("python train_combined.py  --checkpoint_path experiments/combined/cifar/checkpoint.pth --lr 0.01")
+os.system("python train_combined.py  --checkpoint_path experiments/combined/cifar/checkpoint.pth --lr 0.001")
+
+
+######################## CONDITIONED TRAINING ###########################################################################################
+
+# os.system("python train_conditioned.py  --lr 0.1")
+# os.system("python train_conditioned.py  --cifar_checkpoint_path experiments/conditioned/cifar/checkpoint.pth --fashion_mnist_checkpoint_path experiments/conditioned/fashion_mnist/checkpoint.pth --lr 0.01")
+# os.system("python train_conditioned.py  --cifar_checkpoint_path experiments/conditioned/cifar/checkpoint.pth --fashion_mnist_checkpoint_path experiments/conditioned/fashion_mnist/checkpoint.pth --lr 0.001")
