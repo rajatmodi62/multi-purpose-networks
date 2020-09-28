4a78d687d3ee91831ced1297a84a94665bc8fb57
R. Modi <rajatmodi62@gmail.com>
2020-09-28 20:56:50 +0530

    testing for alternate conditioning

--- alternate_conditioning.py
+++ alternate_conditioning.py
@@ -104,7 +104,7 @@ def test():
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader_cifar), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
     acc = 100.*correct/total
     print("Accuracy is",acc, "for dataset",args.dataset, " conditioning label ", embedding_label )
